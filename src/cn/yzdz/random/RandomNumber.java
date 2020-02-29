b26739d9f5b675ede53cdd491aca37aed7d7e346
TrueNine <2394997288@qq.com>
2020-02-29 13:56:53 +0800

    更新了 RandomNumber ,增加了方法 重新编译了 jar 包 已将 jar 包 应用于笔记项目

--- src/cn/yzdz/random/RandomNumber.java
+++ src/cn/yzdz/random/RandomNumber.java
@@ -2,7 +2,7 @@ package cn.yzdz.random;
 
 /**
  * 类作用: 工具类
- * 类说明: 包含一些常用的随机数字,浮点数方法
+ * 类说明: 包含一些常用的随机整数,浮点数方法
  *
  * @author 彭继工
  * @date 2020 - 02 - 28:13:36
@@ -17,21 +17,74 @@ public class RandomNumber {
     }
 
     /**
-     * 返回 0 - max 的随机数
-     * 使用 Math.ramdom 执行
-     * 在 math.random * max 之后进行了 + 1操作
-     * 所以使用时,请考虑情况
-     * 此方法是一个静态方法,所以可以尽管调用
+     * 空方法,如果忘记了调用,则会使用该方法
+     * 该方法返回一个 1 - 10 之间的数值
      *
-     * 加入boolean 变量,使用该布尔变量,可以取到 0
-     * 但请小心 数学异常
+     * @return 整数 int
+     */
+    public static int randomInt() {
+        int defaultNumber = 10;
+        return (int) (Math.random() * defaultNumber) + 1;
+    }
+
+    /**
+     * 该方法为私有方法
+     * 返回一个范围内的值,会取零
+     * 对外私有,尽量少暴露方法
      *
      * @param max 最大值
+     * @return 整数 int
+     */
+    private static int randomInt(int max) {
+        return (int) (Math.random() * max);
+    }
+
+    /**
+     * 公开方法
+     * 根据 max 参数,返回一个 直到 max 的随机整数
+     * isZero 控制是否取零,如果不取零,则直接给予 false,调用私有方法
+     * 如果传入的值 小于等于 0,则会抛出异常
+     *
+     * @param max 最大取值范围
      * @param isZero 是否取零
-     * @return 随机整数 int
+     * @return 整数 int
      */
     public static int randomInt(int max,boolean isZero) {
-        int result = (int) (Math.random() * max);
-        return isZero ? result + 1 : result;
+        // 非零负数排除
+        if (0 >= max) {
+            throw new RuntimeException("数值越界,不能是 0 或 负数");
+        }
+        return isZero ? randomInt(max) : randomInt(max) + 1;
+    }
+
+    /**
+     * 私有方法
+     * 处理一个随机在 beforeRange 和 afterRange 之间的整数
+     * 会取到 0
+     *
+     * @param beforeRange 范围最小值
+     * @param afterRange 范围最大值
+     * @return 整数 int
+     */
+    private static int randomInt(int beforeRange,int afterRange) {
+        return (int) (Math.random() * (afterRange - beforeRange)) + beforeRange;
+    }
+
+    /**
+     * 对外公开方法
+     * 获取一个在 beforeRange 和 afterRange 之间的整数
+     * isZero 控制是否取零,如果不取零,则直接给予 false,调用私有方法
+     * 如果传入的数值 小于等于 0,则会抛出异常
+     *
+     * @param beforeRange 最小值
+     * @param afterRange 最大值
+     * @param isZero 是否取零
+     * @return 整数 int
+     */
+    public static int randomInt(int beforeRange,int afterRange,boolean isZero) {
+        if (0 >= beforeRange || 0 >= afterRange) {
+            throw new RuntimeException("数值越界,不能是 0 或 负数");
+        }
+        return isZero ? randomInt(beforeRange,afterRange) : randomInt(beforeRange,afterRange) + 1;
     }
 }
