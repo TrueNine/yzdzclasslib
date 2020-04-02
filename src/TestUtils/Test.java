09e5d274d54236e6d410022482597ed45abaef4e
TrueNine <2394997288@qq.com>
2020-04-02 16:53:40 +0800

    提交现有的所有文件

--- src/TestUtils/Test.java
+++ src/TestUtils/Test.java
@@ -1,12 +1,5 @@
 package TestUtils;
 
-import cn.how2j.YzdzStringBuffer;
-import cn.yzdz.random.RandomChar;
-import cn.yzdz.random.RandomString;
-import cn.yzdz.random.RandomStringX;
-
-import java.util.ArrayList;
-
 /**
  * 测试类
  * 测试一些工具类的功能
@@ -15,11 +8,7 @@ import java.util.ArrayList;
  * @date 2020-03-09:17:31
  */
 public class Test {
-
-    private Test(){}
-
     public static void main(String[] args) {
-        RandomStringX x = new RandomStringX();
-        System.out.println(x.letter().number().chinese().build(24));
+        System.out.println("测试1");
     }
 }
